e6016b5ec171a2d3d82c7b1865c4e4d35f4c1b5e
Aldair Gutierrez <aldairgguer@gmail.com>
2025-06-02 14:20:21 -0500

    chore: update package.json dependencies and devDependencies; add autoprefixer to postcss.config; include styles directory in tailwind config; implement managementIAService for creating IA management entries

---
+++ app/backend/management-ia/management-ia.ts
@@ -0,0 +1,33 @@
+import { AccessTokenService } from "@/app/api/accesToken";
+import axios from "axios";
+
+export interface ManagementIa {}
+
+const urlBas = process.env.NEXT_PUBLIC_API_URL || "https://api.ejemplo.com";
+const urlBase = `${urlBas}/api/pharma-guide/ia-management`;
+
+export class managementIAService {
+  async createManagementIa(managementIa: ManagementIa) {
+    try {
+      const token = await AccessTokenService.getToken();
+
+      const response = await axios.post(
+        `${urlBase}`,
+        { data: managementIa },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        return response.data;
+      } else {
+        throw new Error("Error en la respuesta del servidor.");
+      }
+    } catch (error: any) {
+      throw new Error(error?.message || "Error al crear el modjelo de IA");
+    }
+  }
+}
